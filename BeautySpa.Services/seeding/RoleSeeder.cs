1a925ad58bd332d2665081ced3bff895a5ea4b82
vuacuhanh <105980840+vuacuhanh@users.noreply.github.com>
2025-04-29 13:59:40 +0700

    Thêm roldeSeeder , Sửa lại roleservice, roleController chuẩn bussines, sủa lại program.cs

---
+++ BeautySpa.Services/seeding/RoleSeeder.cs
@@ -0,0 +1,35 @@
+﻿using BeautySpa.Contract.Repositories.Entity;
+using Microsoft.AspNetCore.Identity;
+
+namespace BeautySpa.Services.seeding
+{
+    public static class RoleSeeder
+    {
+        private static readonly List<string> DefaultRoles = new()
+        {
+            "Admin",
+            "Customer",
+            "Provider"
+        };
+
+        public static async Task SeedRolesAsync(RoleManager<ApplicationRoles> roleManager)
+        {
+            foreach (var roleName in DefaultRoles)
+            {
+                if (!await roleManager.RoleExistsAsync(roleName))
+                {
+                    var role = new ApplicationRoles
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = roleName,
+                        NormalizedName = roleName.ToUpper(),
+                        CreatedTime = DateTime.UtcNow,
+                        ConcurrencyStamp = Guid.NewGuid().ToString()
+                    };
+
+                    await roleManager.CreateAsync(role);
+                }
+            }
+        }
+    }
+}
