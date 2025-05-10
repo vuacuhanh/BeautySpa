b1adcc9c7b002709d33a3e6acd5f50c5e7c2e1f5
vuacuhanh <105980840+vuacuhanh@users.noreply.github.com>
2025-05-11 01:09:14 +0700

    fix program

--- BeautySpa/Program.cs
+++ BeautySpa/Program.cs
@@ -151,12 +151,12 @@ using (var scope = app.Services.CreateScope())
     await RankSeeder.SeedRanksAsync(serviceProvider);
 }
 
-// 11. Middleware
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "BeautySpa API v1");
+    c.RoutePrefix = "swagger"; // truy cập /swagger
+});
 
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseMiddleware<LoggingMiddleware>();
