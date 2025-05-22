116255054fe30c45b4bcc288d15998bbdaf38920
vuacuhanh <105980840+vuacuhanh@users.noreply.github.com>
2025-05-22 20:00:48 +0700

    Sửa lại lần cuối thuộc tính avarta and gender

--- BeautySpa.Services/Mapper/UserMapping.cs
+++ BeautySpa.Services/Mapper/UserMapping.cs
@@ -17,6 +17,8 @@ namespace BeautySpa.Services.Mapper
                 .ForMember(dest => dest.DistrictId, opt => opt.MapFrom(src => src.UserInfor.DistrictId))
                 .ForMember(dest => dest.DistrictName, opt => opt.MapFrom(src => src.UserInfor.DistrictName))
                 .ForMember(dest => dest.DateOfBirth, opt => opt.MapFrom(src => src.UserInfor != null ? src.UserInfor.DayofBirth : DateTime.MinValue))
+                .ForMember(dest => dest.AvatarUrl, opt => opt.MapFrom(src => src.UserInfor.AvatarUrl))
+                .ForMember(dest => dest.Gender, opt => opt.MapFrom(src => src.UserInfor.Gender))
                 .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => src.Status == "active"))
                 .ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null));
 
@@ -24,6 +26,7 @@ namespace BeautySpa.Services.Mapper
             CreateMap<UserInfor, GETUserInfoModelView>()
                 .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.FullName))
                 .ForMember(dest => dest.AddressDetail, opt => opt.MapFrom(src => src.AddressDetail))
+                .ForMember(dest => dest.Gender, opt => opt.MapFrom(src => src.Gender))
                 .ForMember(dest => dest.ProvinceId, opt => opt.MapFrom(src => src.ProvinceId))
                 .ForMember(dest => dest.ProvinceName, opt => opt.MapFrom(src => src.ProvinceName))
                 .ForMember(dest => dest.DistrictId, opt => opt.MapFrom(src => src.DistrictId))
@@ -37,6 +40,7 @@ namespace BeautySpa.Services.Mapper
             CreateMap<PUTUserModelViews, UserInfor>()
                 .ForMember(dest => dest.Id, opt => opt.Ignore()) // Ignore Id tránh lỗi primary key
                 .ForMember(dest => dest.AvatarUrl, opt => opt.MapFrom(src => src.AvatarUrl))
+                .ForMember(dest => dest.Gender, opt => opt.MapFrom(src => src.Gender))
                 .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.FullName))
                 .ForMember(dest => dest.AddressDetail, opt => opt.MapFrom(src => src.AddressDetail))
                 .ForMember(dest => dest.ProvinceId, opt => opt.MapFrom(src => src.ProvinceId))
