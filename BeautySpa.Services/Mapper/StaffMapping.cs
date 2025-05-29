61a389572f8bcfb5cff0178113e90c84036203b3
vuacuhanh <105980840+vuacuhanh@users.noreply.github.com>
2025-05-29 22:27:38 +0700

    staff update

--- BeautySpa.Services/Mapper/StaffMapping.cs
+++ BeautySpa.Services/Mapper/StaffMapping.cs
@@ -6,13 +6,15 @@ public class StaffMapping : Profile
 {
     public StaffMapping()
     {
+        CreateMap<Staff, GETStaffModelView>()
+            .ForMember(dest => dest.ServiceCategoryIds,
+                opt => opt.MapFrom(src => src.StaffServiceCategories.Select(x => x.ServiceCategoryId)))
+            .ForMember(dest => dest.ServiceCategoryNames,
+                opt => opt.MapFrom(src => src.StaffServiceCategories.Select(x => x.ServiceCategory.CategoryName)));
+
         CreateMap<POSTStaffModelView, Staff>()
             .ForMember(dest => dest.StaffServiceCategories, opt => opt.Ignore());
-
         CreateMap<PUTStaffModelView, Staff>()
             .ForMember(dest => dest.StaffServiceCategories, opt => opt.Ignore());
-
-        CreateMap<Staff, GETStaffModelView>()
-            .ForMember(dest => dest.ServiceCategoryIds,opt => opt.MapFrom(s => s.StaffServiceCategories.Select(x => x.ServiceCategoryId)));
     }
 }
