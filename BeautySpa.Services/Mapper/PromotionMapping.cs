515e7e2e54895a5c329b5604aab95ea131549bc5
vuacuhanh <105980840+vuacuhanh@users.noreply.github.com>
2025-05-29 18:02:25 +0700

    hehe

--- BeautySpa.Services/Mapper/PromotionMapping.cs
+++ BeautySpa.Services/Mapper/PromotionMapping.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using BeautySpa.Contract.Repositories.Entity;
+using BeautySpa.Core.Utils;
 using BeautySpa.ModelViews.PromotionModelViews;
 
 namespace BeautySpa.Services.Mapper
@@ -9,8 +10,9 @@ namespace BeautySpa.Services.Mapper
         public PromotionMapping()
         {
             CreateMap<Promotion, GETPromotionModelView>()
-                .ForMember(dest => dest.ProviderName, opt => opt.MapFrom(src => src.Provider!.UserInfor!.FullName));
-
+                .ForMember(dest => dest.ProviderName, opt => opt.MapFrom(src => src.Provider!.UserInfor!.FullName))
+                .ForMember(dest => dest.StartDate, opt => opt.MapFrom(src => TimeHelper.ConvertToUtcPlus7(src.StartDate)))
+                .ForMember(dest => dest.EndDate, opt => opt.MapFrom(src => TimeHelper.ConvertToUtcPlus7(src.EndDate)));
             CreateMap<POSTPromotionModelView, Promotion>();
             CreateMap<PUTPromotionModelView, Promotion>();
         }
--- BeautySpa.Services/Mapper/ServicePromotionMapping.cs
+++ BeautySpa.Services/Mapper/ServicePromotionMapping.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using BeautySpa.Contract.Repositories.Entity;
+using BeautySpa.Core.Utils;
 using BeautySpa.ModelViews.ServicePromotionModelView;
 
 namespace BeautySpa.Services.Mapper
@@ -9,7 +10,11 @@ namespace BeautySpa.Services.Mapper
         public ServicePromotionMapping()
         {
             CreateMap<ServicePromotion, GETServicePromotionModelView>()
-                .ForMember(dest => dest.ServiceName, opt => opt.MapFrom(src => src.Service!.ServiceName));
+                .ForMember(dest => dest.ServiceName, opt => opt.MapFrom(src => src.Service!.ServiceName))
+                .ForMember(dest => dest.StartDate, opt => opt.MapFrom(src =>
+                    TimeHelper.ConvertToUtcPlus7(src.StartDate)))
+                .ForMember(dest => dest.EndDate, opt => opt.MapFrom(src =>
+                    TimeHelper.ConvertToUtcPlus7(src.EndDate)));
 
             CreateMap<POSTServicePromotionModelView, ServicePromotion>();
             CreateMap<PUTServicePromotionModelView, ServicePromotion>();
