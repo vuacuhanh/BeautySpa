c1d1f0b6961ff2a48013cb7e1e7e9b4d677d4d8e
vuacuhanh <105980840+vuacuhanh@users.noreply.github.com>
2025-05-30 12:36:32 +0700

    fix spa

---
+++ BeautySpa.Services/Validations/SpaBranchLocationModelViewValidator/POSTSpaBranchLocationModelViewValidator.cs
@@ -0,0 +1,28 @@
+﻿using BeautySpa.ModelViews.LocationModelViews;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautySpa.Services.Validations.SpaBranchLocationModelViewValidator
+{
+    public class POSTSpaBranchLocationModelViewValidator : AbstractValidator<POSTSpaBranchLocationModelView>
+    {
+        public POSTSpaBranchLocationModelViewValidator()
+        {
+            RuleFor(x => x.BranchName)
+                .NotEmpty().WithMessage("Branch name is required.");
+
+            RuleFor(x => x.ProvinceId)
+                .NotEmpty().WithMessage("Province is required.");
+
+            RuleFor(x => x.DistrictId)
+                .NotEmpty().WithMessage("District is required.");
+
+            RuleFor(x => x.Street)
+                .MaximumLength(200).WithMessage("Street must be at most 200 characters.");
+        }
+    }
+}
