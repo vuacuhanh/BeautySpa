a89d6db9bc7a0e25d4e40f275b46822068813a6a
vuacuhanh <105980840+vuacuhanh@users.noreply.github.com>
2025-06-10 15:50:34 +0700

    Thống kê

---
+++ BeautySpa.Contract.Services/Interface/IStatisticService.cs
@@ -0,0 +1,16 @@
+﻿using BeautySpa.Core.Base;
+using BeautySpa.ModelViews.StatisticModelViews;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautySpa.Contract.Services.Interface
+{
+    public interface IStatisticService
+    {
+        Task<BaseResponseModel<StatisticResultModelView>> GetAdminStatisticsAsync(StatisticFilterModelView filter);
+        Task<BaseResponseModel<StatisticResultModelView>> GetProviderStatisticsAsync(StatisticFilterModelView filter, Guid providerId);
+    }
+}
